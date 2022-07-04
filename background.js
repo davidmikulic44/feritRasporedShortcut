69e9ff5530bfa60407a3aabbd3ce30f415b0abe7
David Mikulić <74313441+davidmikulic44@users.noreply.github.com>
2022-07-04 21:42:39 +0200

    Update background.js

--- background.js
+++ background.js
@@ -17,13 +17,14 @@ var yyyy = today.getFullYear();
 today = yyyy + '-' + mm + '-' + dd;
 
 console.log(today);
+console.log(yyyy);
 
 chrome.storage.sync.get('code', function(result){
   codeString = result.code;
 });
 
 chrome.browserAction.onClicked.addListener(function(activeTab){
-  var newURL = "https://www.ferit.unios.hr/2021/studenti/raspored-nastave-i-ispita/"+today+"/"+codeString+"#raspored";
+  var newURL = "https://www.ferit.unios.hr/"+(yyyy-1)+"/studenti/raspored-nastave-i-ispita/"+today+"/"+codeString+"#raspored";
   console.log(newURL);
   chrome.tabs.create({ url: newURL });
 });

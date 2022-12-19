9c73b6b0b30fc382d08629990e80e9b51f798303
David Mikulić <74313441+davidmikulic44@users.noreply.github.com>
2022-12-19 16:10:03 +0100

    Update background.js

--- feritRasporedShortcut-main/background.js
+++ feritRasporedShortcut-main/background.js
@@ -1,6 +1,10 @@
 var codeString = '';
+
 var today = new Date();
 var day = today.getDay();
+console.log(day);
+
+
 
 if(day===0){
   today.setDate(today.getDate()+1);
@@ -9,28 +13,20 @@ if(day===0){
 var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0');
 var yyyy = today.getFullYear();
-var marginDate = new Date();
-var todayDate = new Date();
 
-todayDate.setHours(0,0,0,0);
-
-marginDate.setFullYear(yyyy);
-marginDate.setDate(30);
-marginDate.setMonth(8);
-marginDate.setHours(0,0,0,0);
 today = yyyy + '-' + mm + '-' + dd;
+
+console.log(today);
+console.log(yyyy);
 
 chrome.storage.sync.get('code', function(result){
   codeString = result.code;
 });
 
-if(marginDate>todayDate){
-  --yyyy;
-}
+
 
 chrome.browserAction.onClicked.addListener(function(activeTab){
-  var newURL = "https://www.ferit.unios.hr/"+yyyy
-  +"/studenti/raspored-nastave-i-ispita/"+today+"/"
-  +codeString+"#raspored";
+  var newURL = "https://www.ferit.unios.hr/studenti/raspored-nastave-i-ispita/"+today+"/"+codeString+"#raspored";
+  console.log(newURL);
   chrome.tabs.create({ url: newURL });
 });
